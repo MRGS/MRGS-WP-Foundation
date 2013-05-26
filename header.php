4deddac87a55f205d6cf4afec98f434afdeee62d
Stephen Ascher <sixsecondfiesta@gmail.com>
2013-05-26 19:48:02 -0400

    well, a picture shows up.

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-
+	<?php /* maybe in smallest nav mode?
 	<nav class="top-bar">
 		<ul class="title-area">
 			<li class="name"><h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></h1></li>
@@ -40,17 +40,20 @@
 			<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'menu_class' => 'left', 'container' => '', 'fallback_cb' => 'foundation_page_menu', 'walker' => new foundation_navigation() ) ); ?>
 		</section>
 	</nav>
+	*/ ?>
 
-	<?php $header =  get_header_textcolor();
-	if ( $header !== "blank" ) : ?>
-	<header class="site-header" <?php $header_image = get_header_image(); if ( ! empty( $header_image ) ) : ?> style="background:url('<?php echo esc_url( $header_image ); ?>');" <?php endif; ?>>
+	<header class="site-header">
 		<div class="row">
-			<div class="large-12 columns">
-				<h2><a style="color:#<?php header_textcolor(); ?>;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'description' ); ?></a></h2>
+			<div class="large-9 columns">
+				<h1 class="right"><a style="color:#<?php header_textcolor(); ?>;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			</div>
+			<div class="large-3 columns">
+				<a style="color:#<?php header_textcolor(); ?>;" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<img src="<?php echo get_stylesheet_directory_uri().'/img/mergsey.png'; ?>" alt="MRGS/SLMR">
+				</a>
 			</div>
 		</div>
 	</header>
-	<?php endif; ?>
 
 <!-- Begin Page -->
 <div class="row">
